a9f8b2e3d1c7a8546c3104a71c8c202c6a673482
Oladayo <oladayoafolabi@outlook.com>
2025-03-25 12:08:14 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:mortgage_payment_starter_app/ui/mortgage_app.dart';
+
+void main() => runApp(
+  new MaterialApp(debugShowCheckedModeBanner: false, home: MortgageApp()),
+);
